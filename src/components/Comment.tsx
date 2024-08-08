1a4f9904a2d602707171bb7c0d10f4c0e33638c6
o0Mink0o <141802449+o0Mink0o@users.noreply.github.com>
2024-08-08 15:12:44 +0700

    sdsd

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,15 +1,17 @@
 "use client";
+import { CommentProps } from "@/libs/types";
 export default function Comment({
-  userImagePath,
+   userImagePath,
   username,
   commentText,
   likeNum,
   replies,
-}) {
+}: CommentProps) {
   return (
     <div>
       your code for Comment component here ...
       {/* You can use map-loop to render Reply component here */}
+    
     </div>
   );
 }
--- src/components/PostOwnner.tsx
+++ src/components/PostOwnner.tsx
@@ -1,4 +1,31 @@
 "use client";
+
 export default function PostOwnner({}) {
-  return <div>Your code for PostOwner component ...</div>;
+  return <div>
+    <div className="vstack gap-3">
+          <div className="d-flex align-items-center gap-3">
+            <img
+              src="“public/profileImages/660610800.png"
+              width="48"
+              height="48"
+              className="rounded-circle"
+              style={{ objectFit: "cover" }}
+            />
+            <span className="fw-semibold fs-5 text-white">
+              Sorawit Yokthlemthae 660610800
+            </span>
+          </div>
+
+          <span className="text-white">
+            Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207
+          </span>
+
+          <div className="d-flex align-items-center gap-1">
+            <img src="/like.svg" width={20}></img>
+            <span style={{ color: "#B0B3B8" }}>100 คน</span>
+          </div>
+          <hr className="m-0 border" />
+        </div>
+
+  </div>;
 }
